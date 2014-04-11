6e25563e790cf94a97e29562e5f5c77b88b6a98c
Md Towhidul Chowdhury <mac9908@rit.edu>
2014-04-11 19:53:05 -0400

    Working on the login files.

---
+++ app/src/main/java/com/example/umm/app/login_menu.java
@@ -0,0 +1,34 @@
+package com.example.umm.app;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by towhi_000 on 4/11/14.
+ */
+public class login_menu extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    private void addListenerOnButtons(){
+
+        final Context context = this;
+        Button login_button = (Button) findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, user_credentials.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/umm/app/user_credentials.java
@@ -0,0 +1,20 @@
+package com.example.umm.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+/**
+ * Created by towhi_000 on 4/11/14.
+ */
+public class user_credentials extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_credentials);
+    }
+}
