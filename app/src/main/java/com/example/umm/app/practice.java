e475a602b21c37a0eddde62a227541ec66635812
Isaac Kunkel <iak5171@rit.edu>
2014-04-11 21:53:52 -0400

    Cleaning practice screen and activity

---
+++ app/src/main/java/com/example/umm/app/practice.java
@@ -0,0 +1,50 @@
+package com.example.umm.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class practice extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.practice);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.layout.practice, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void beginPractice(View v) {
+
+    }
+
+    public void logout(View v) {
+        new login_menu();
+    }
+
+    public void toMainMenu() {
+        new main_menu();
+    }
+
+}
