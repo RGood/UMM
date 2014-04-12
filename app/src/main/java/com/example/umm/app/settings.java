87ffb5440ec919de292fd01f70734bb97656f029
JeremyVargas <Jeremy@129.21.139.49>
2014-04-11 22:55:12 -0400

    fixed reference to settings.xml in main settings screen

--- app/src/main/java/com/example/umm/app/settings.java
+++ app/src/main/java/com/example/umm/app/settings.java
@@ -10,7 +10,7 @@ public class settings extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settings);
     }
 
 
