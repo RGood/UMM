9d801748e8ea1eaf348a7cbf95bdedcc9fec3f13
Md Towhidul Chowdhury <mac9908@rit.edu>
2014-04-11 21:07:16 -0400

    User credentials fixed.

--- app/src/main/java/com/example/umm/app/user_credentials.java
+++ app/src/main/java/com/example/umm/app/user_credentials.java
@@ -18,6 +18,7 @@ public class user_credentials extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_credentials);
+        addListenerOnButtons();
     }
 
     public void addListenerOnButtons(){
