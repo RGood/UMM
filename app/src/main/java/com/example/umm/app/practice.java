c33cf9a09046094a2fa3cc8dd1e6f0370c819f69
Isaac Kunkel <iak5171@rit.edu>
2014-04-12 02:45:39 -0400

    finished practice

--- app/src/main/java/com/example/umm/app/practice.java
+++ app/src/main/java/com/example/umm/app/practice.java
@@ -17,6 +17,7 @@ public class practice extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.practice);
+        addListenerOnButtons();
     }
 
 
